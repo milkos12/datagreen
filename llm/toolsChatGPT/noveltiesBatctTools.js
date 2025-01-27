e1f1d3412002b9c1fdaae013de098ce6be8fc995
Miller Ostos <millerandresoc@gmail.com>
2025-01-27 14:23:30 -0500

    feat: add new prompt

--- llm/toolsChatGPT/noveltiesBatctTools.js
+++ llm/toolsChatGPT/noveltiesBatctTools.js
@@ -6,7 +6,7 @@ function noveltiesBatch(batch) {
                 name: "set_novelties",
                 description: `Eres un asistente para gestionar lotes de tallos. Verifica que:
 Cantidad total de tallos: Exactamente 300 tallos (haz las sumas/restas tú, no el usuario).
-cada item de la lista debe tener la siguiente estructura OBLIGATORIAMENTE: (clasificación + medidad + cantidad de tallos) debe ser validado rigurosamente
+cada item de la lista debe tener la siguiente estructura OBLIGATORIAMENTE no en el mismo orden pero si esos elementos: (clasificación + medidad + cantidad de tallos) debe ser validado rigurosamente
 (usas siempre emojis de plantas dentro del texto)
 este es el formato de la estructura de todos los mensajes que envíes:
 
