025889f85d069fa4dfbbfe818cf0927291e63f71
Miller Ostos <millerandresoc@gmail.com>
2025-02-04 17:28:27 -0500

    refactor:#11 fix bug in parameter to ID phone user

--- services/MessageService.js
+++ services/MessageService.js
@@ -2,10 +2,10 @@ const { MessagePersistence } = require('../models');
 
 const MessageService = {
   shouldShowMenu: async (userId) => !(await MessagePersistence.findOne({ where: { user_id: userId } })),
-  createPersistance: async (userId, batchName) => await MessagePersistence.create({
+  createPersistance: async (userId, batchName) => MessagePersistence.create({
     user_id: userId,
     messages: [{ role: 'user', content: 'Inicio del chat' }],
-    activity_id: 'd683fe0a-9c6b-4cclave única',
+    activity_id: 'd683fe0a-9c6b-4cbe-b131-f647c53fc215',
     whatsapp_id: batchName
   }),
 };
